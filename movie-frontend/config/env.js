8659a8e01fd5767bf66a93eeb9c0a5a78187edbb
Americool <aberanderson@gmail.com>
2017-07-30 03:15:25 -0400

    react seems to be working too

--- movie-frontend/config/env.js
+++ movie-frontend/config/env.js
@@ -73,6 +73,8 @@ function getClientEnvironment(publicUrl) {
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
+
+        //You don't need the env for IMDB anymore
         PUBLIC_URL: publicUrl,
         IMDB_KEY: process.env.IMDB
       }
